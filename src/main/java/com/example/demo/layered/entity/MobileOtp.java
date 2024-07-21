e7751df877c5cb40caf4150273843dd1dd38a95e
shin-jingyu <wlsrb97@naver.com>
2024-07-22 03:56:38 +0900

    chore(layered): add comment about excluding getter/setter

--- src/main/java/com/example/demo/layered/entity/MobileOtp.java
+++ src/main/java/com/example/demo/layered/entity/MobileOtp.java
@@ -8,6 +8,9 @@ import org.springframework.data.redis.core.index.Indexed;
 
 @RedisHash("otp")
 public class MobileOtp {
+    // id는 임의로 바꾸면 안되기 떄문에 private
+    // id를 제외한 다른 필드를 public으로 한 이유는
+    // IDE의 발전으로, 불필요한 경우까지 getter/setter로 통일할 필요가 없기 때문이다.
 
     @Id
     @Getter
